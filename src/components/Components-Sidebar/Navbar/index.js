d56df9110f1c78bd9365720442561af64d7f3393
IVaNaRsenoV <37076298+IVaNaRsenoV@users.noreply.github.com>
2022-04-14 15:33:31 +0300

    Боковое меню, следующий этап - это стилизация меню

--- src/components/Components-Sidebar/Navbar/index.js
+++ src/components/Components-Sidebar/Navbar/index.js
@@ -1,39 +1,12 @@
-import { useState } from "react";
-import { useList } from "../../../hooks/useList";
-
-const Menu = ({ items }) => {
-  const [displayChildren, setDisplayChildren] = useState({});
-
-  return (
-    <ul>
-      {items.map((item) => {
-        return (
-          <li key={item.title}>
-            {item.title}{" "}
-            {item.children && (
-              <button
-                onClick={() => {
-                  setDisplayChildren({
-                    ...displayChildren,
-                    [item.title]: !displayChildren[item.title],
-                  });
-                }}
-              >
-                {displayChildren[item.title] ? "-" : "+"}
-              </button>
-            )}
-            {displayChildren[item.title] && item.children && (
-              <Menu items={item.children} />
-            )}
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+import { Menu } from "./Menu";
+import { menu } from "./data/data";
 
 export const Navbar = () => {
-  const data = useList("/post", "POST");
-  console.log(data);
-  return <Menu items={data} />;
+  return (
+    <>
+      <input type="text" placeholder="Поиск..." />
+      <input type="submit" />
+      <Menu items={menu} />
+    </>
+  );
 };
